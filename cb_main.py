765ebcf9a3713c95223337ec0ae33ffae5bb7dca
Maenswirony <154528712+maenswirony@users.noreply.github.com>
2024-02-19 12:14:52 -0500

    Update cb_main.py

--- cb_main.py
+++ cb_main.py
@@ -1,3 +1,22 @@
+'''
+    Culture Blocks is a public goods project to build better tools for 
+    social connection and to emerge collective wisdom.
+    Copyright (C) 2024  maenswirony
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import os
 import re
 import random
